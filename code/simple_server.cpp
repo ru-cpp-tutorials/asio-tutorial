0cf7cd208fcca1ee3baadfce17001fc7b4bcd21c
Daniil Shvalov <shvalovich@gmail.com>
2022-07-13 02:08:23 +0300

    feat: add simple server section

---
+++ code/simple_server.cpp
@@ -0,0 +1,19 @@
+#include <boost/asio.hpp>
+
+int main() {
+    std::uint16_t port = 15001;
+
+    boost::asio::io_context io_context;
+    boost::asio::ip::udp::endpoint receiver(boost::asio::ip::udp::v4(), port);
+    boost::asio::ip::udp::socket socket(io_context, receiver);
+
+    while (true) {
+        char buffer[65536];
+        boost::asio::ip::udp::endpoint sender;
+        std::size_t bytes_transferred =
+            socket.receive_from(boost::asio::buffer(buffer), sender);
+        socket.send_to(boost::asio::buffer(buffer, bytes_transferred), sender);
+    }
+
+    return 0;
+}
